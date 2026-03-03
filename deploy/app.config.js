def5381006bb26ed65ad5146377497867a93477f
Ian Walls <mail.sekjal@gmail.com>
2026-03-03 11:46:24 -0500

    Refactor app.config.js to load build.json earlier

--- deploy/app.config.js
+++ deploy/app.config.js
@@ -1,10 +1,9 @@
 module.exports = () => {
     const version = require('../version.json');
+    const build = require('build.json');
     let versionAsInt = build['build'];
     versionAsInt = parseInt(versionAsInt, 10);
-
-    const build = require('build.json');
-
+     
     const app = require('app.json');
 
     const googleApiKeyApple = process.env.GOOGLE_API_KEY_APPLE;
